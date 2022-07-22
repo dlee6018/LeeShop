d5e1ce8f93dca2e04937ede2e356e712d3c61e57
David <dlee6018@Davids-Mac-mini.local>
2022-07-21 20:48:48 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ import colors from 'colors'
 import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
@@ -50,7 +49,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5100
+const PORT = process.env.PORT || 5200
 
 app.listen(
   PORT,
