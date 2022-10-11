ae5140a4d0a1ea5ab7085d7f9fb609077c2b6962
David Lee <dlee6018@gmail.com>
2022-10-10 17:42:29 -0700

    path

--- backend/server.ts
+++ backend/server.ts
@@ -20,35 +20,36 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/upload", uploadRoutes);
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
-app.get("/api/config/paypal", (req, res) =>
+app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
-);
+)
 
-// const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+const dirname = path.resolve()
+app.use('/uploads', express.static(path.join(dirname, '/uploads')))
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(dirname, '/frontend/build')))
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(dirname, 'frontend', 'build', 'index.html'))
+  )
 } else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
 
 app.get("/test", (req, res) => {
   res.send("test working");
 });
+
 app.use(notFound);
 app.use(errorHandler);
 
